465b603aa6ce10187ccc56047684bcfb0d53b8fa
Jeongmin39 <80705450+Jeongmin39@users.noreply.github.com>
2024-04-04 12:24:30 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 238.68 ms, Memory: 44.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -0,0 +1,24 @@
+from operator import itemgetter
+
+def solution(food_times, k):
+    foods = []
+    n = len(food_times)
+    for i in range(n):
+        foods.append((food_times[i], i+1))
+    foods.sort()
+    
+    previous_time = 0
+    for i, food in enumerate(foods):
+        diff = food[0] - previous_time
+        if diff != 0:
+            spend_time = diff * n
+            if spend_time <= k:
+                k -= spend_time
+                previous_time = food[0]
+            else:
+                k %= n
+                sublist = sorted(foods[i:], key = itemgetter(1))
+                return sublist[k][1]
+        n -= 1
+        
+    return -1 
