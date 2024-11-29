f42f03f4c1c2b1f07fd09a551d8307c596ba7116
Jeongmin39 <80705450+Jeongmin39@users.noreply.github.com>
2024-11-29 16:32:41 +0900

    Update 연결 요소의 개수.py

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -25,3 +25,37 @@ for i in range(1, n+1):
         dfs(graph, i, visited)
         
 print(count)
+
+"""
+반복문 이용하여 처리
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(start):
+    stack = [start]
+    while stack:
+        v = stack.pop()
+        if not visited[v]:
+            visited[v] = True
+            for i in graph[v]:
+                if not visited[i]:
+                    stack.append(i)
+                    
+count = 0
+for i in range(1, n+1):
+    if not visited[i]:
+        count += 1
+        dfs(i)
+        
+print(count)
+"""
