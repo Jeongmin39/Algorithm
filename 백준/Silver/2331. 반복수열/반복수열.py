b43f6ba494c8c7107b35f003a7f1f33d36f14055
Jeongmin39 <80705450+Jeongmin39@users.noreply.github.com>
2024-12-05 21:11:58 +0900

    Update 반복수열.py

--- 백준/Silver/2331. 반복수열/반복수열.py
+++ 백준/Silver/2331. 반복수열/반복수열.py
@@ -17,3 +17,29 @@ while True:
         current_num = num
         
 print(len(nums))
+
+"""
+dfs 사용
+
+A, P = map(int, input().split())
+visited = [0] * 236197 # 최댓값 9 ** 5 + 9 ** 5 + 9 ** 5 + 9 ** 5
+count = 1
+
+def cal(A, P):
+    result = 0
+    for i in str(A):
+        result += int(i) ** P
+    return result
+
+def dfs(A, P, count, visited):
+    
+    if visited[A] != 0:
+        return visited[A] - 1
+    
+    visited[A] = count
+    count += 1
+    result = cal(A, P)
+    return dfs(result, P, count, visited)
+
+print(dfs(A, P, count, visited))
+"""
