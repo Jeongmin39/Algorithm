f82c814b9a727b3d9004ee90503539c13dbba57a
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2024-12-27 18:02:11 +0900

    Update 네트워크.py

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,3 +1,4 @@
+# DFS
 def dfs(n, com, computers, visited):
     visited[com] = True
     for i in range(n):
@@ -13,3 +14,30 @@ def solution(n, computers):
             dfs(n, com, computers, visited)
             answer += 1
     return answer
+
+"""
+BFS
+
+from collections import deque
+
+def bfs(n, com, computers, visited):
+    queue = deque([com])
+    visited[com] = True
+    while queue:
+        com = queue.popleft()
+        visited[com] = True
+        for i in range(n):
+            if i != com and computers[com][i] ==1 :
+                if not visited[i]:
+                    queue.append(i)
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    for com in range(n):
+        if not visited[com]:
+            bfs(n, com, computers, visited)
+            answer += 1
+    return answer
+    
+"""
