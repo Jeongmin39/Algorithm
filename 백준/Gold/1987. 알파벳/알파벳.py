9db11b8e82ceab04926fc91911ff8f6d70ff24a9
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2025-02-07 22:42:32 +0900

    [Gold IV] Title: 알파벳, Time: 820 ms, Memory: 52888 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -7,16 +7,24 @@ board = [list(input().rstrip()) for _ in range(r)]
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
-def dfs(x, y, history, count):
-    max_count = count
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < r and 0 <= ny < c and board[nx][ny] not in history:
-            history.add(board[nx][ny])
-            max_count = max(max_count, dfs(nx, ny, history, count + 1))
-            history.remove(board[nx][ny])
-            
-    return max_count
+def bfs():
+    max_count = 0
+    queue = set()
+    queue.add((0, 0, board[0][0]))
+
+    while queue:
+        x, y, history = queue.pop()
+        max_count = max(max_count, len(history))
+
+        if max_count == 26:
+            return max_count
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < r and 0 <= ny < c and board[nx][ny] not in history:
+                queue.add((nx, ny, history + board[nx][ny]))
     
-print(dfs(0, 0, {board[0][0]}, 1))
+    return max_count
+
+print(bfs())
