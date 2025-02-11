7407e4aee54bf2e0ab587f29aef7ef68826d73c1
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2025-02-11 20:37:25 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,43 +1,14 @@
-# DFS
-def dfs(n, com, computers, visited):
-    visited[com] = True
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
     for i in range(n):
-        if i != com and computers[com][i] == 1:
-            if not visited[i]:
-                dfs(n, i, computers, visited)
-    
-def solution(n, computers):
-    answer = 0
-    visited = [False] * n
-    for com in range(n):
-        if not visited[com]:
-            dfs(n, com, computers, visited)
+        if not visited[i]:
             answer += 1
+            dfs(i, n, computers, visited)
     return answer
 
-"""
-BFS
-
-from collections import deque
-
-def bfs(n, com, computers, visited):
-    queue = deque([com])
-    visited[com] = True
-    while queue:
-        com = queue.popleft()
-        visited[com] = True
-        for i in range(n):
-            if i != com and computers[com][i] ==1 :
-                if not visited[i]:
-                    queue.append(i)
-
-def solution(n, computers):
-    answer = 0
-    visited = [False] * n
-    for com in range(n):
-        if not visited[com]:
-            bfs(n, com, computers, visited)
-            answer += 1
-    return answer
-    
-"""
+def dfs(v, n, computers, visited):
+    visited[v] = True
+    for i in range(n):
+        if computers[v][i] == 1 and i != v and not visited[i]:
+            dfs(i, n, computers, visited)    
