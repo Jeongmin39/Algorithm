4833c86cbc1249f443cc1c94c0c447db00c5aff5
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2025-02-12 14:14:46 +0900

    [Silver II] Title: 과자 나눠주기, Time: 4112 ms, Memory: 146176 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+snacks = list(map(int, input().split()))
+
+left = 1
+right = max(snacks)
+
+result = 0
+while left <= right:
+    total = 0
+    mid = (left + right) // 2
+
+    for snack in snacks:
+        if snack >= mid:
+            total += snack // mid
+            if total > m:
+                break
+    
+    if total < m:
+        right = mid - 1
+    else:
+        result = mid
+        left = mid + 1
+
+print(result)
