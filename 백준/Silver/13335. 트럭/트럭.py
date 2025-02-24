614eab52c1c7b09b4d98e6edb9eab72df93142b8
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2025-02-24 16:31:14 +0900

    [Silver I] Title: 트럭, Time: 64 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, w, l = map(int, input().split())
+trucks = list(map(int, input().split()))
+
+time = 0
+bridge = [0] * w
+while bridge:
+    time += 1
+    bridge.pop(0)
+    if trucks:
+        if sum(bridge) + trucks[0] <= l:
+            bridge.append(trucks.pop(0))
+        else:
+            bridge.append(0)
+
+print(time)
