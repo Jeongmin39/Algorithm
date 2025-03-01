778001b51e7058a555d361186a7bbb63449349ac
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2025-03-02 00:54:59 +0900

    [Gold II] Title: 구간 자르기, Time: 696 ms, Memory: 71968 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.py
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.py
@@ -1,26 +1,30 @@
 n, k = map(int, input().split())
-vertical = [0] * 1000001
+vertical = [0] * 1000002
 
 for _ in range(n):
     start, end = map(int, input().split())
-    for i in range(start, end):
-        vertical[i] += 1
+    vertical[start + 1] += 1
+    vertical[end + 1] -= 1
+
+# 누적합 계산
+for i in range(1, len(vertical)):
+    vertical[i] += vertical[i-1]
 
 left, right, total = 0, 0, 0
 flag = False
 
-while left <= right < 1000001:
+while right < 1000001:
     if total == k:
         flag = True
         break
     elif total < k:
-        total += vertical[right]
         right += 1
+        total += vertical[right]
     else:
-        total -= vertical[left]
         left += 1
+        total -= vertical[left]
 
 if flag:
     print(left, right)
 else:
-    print("0 0")
+    print(0, 0)
