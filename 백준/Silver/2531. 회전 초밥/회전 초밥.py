0f7cd5be5dd69321ee079dfd744d65eb180678db
한정민 <80705450+Jeongmin39@users.noreply.github.com>
2025-03-02 02:43:25 +0900

    [Silver I] Title: 회전 초밥, Time: 116 ms, Memory: 35352 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,27 +1,27 @@
 import sys
-input = sys.stdin.readline
+from collections import defaultdict
 
+input = sys.stdin.readline
 n, d, k, c = map(int, input().split())
 sushi = [int(input()) for _ in range(n)]
-sushi.extend(sushi[:k-1])
+sushi.extend(sushi)
 
-i = 0
+d = defaultdict(int)
+start, end = 0, 0
 result = 0
-while i < n:
-    l = len(set(sushi[i:i+k]))
-    if l == k:
-        if c not in sushi[i:i+k]:
-            result = max(result, k + 1)
-            break
-        else:
-            result = max(result, k)
 
-    if l < k:
-        if c not in sushi[i:i+k]:
-            result = max(result, l + 1)
-        else:
-            result = max(result, l)
-    
-    i += 1
+while end < k:
+    d[sushi[end]] += 1
+    end += 1
+d[c] += 1 # 쿠폰 초밥 추가
+
+while end < len(sushi):
+    result = max(result, len(d))
+    d[sushi[start]] -= 1
+    if d[sushi[start]] == 0:
+        del d[sushi[start]]
+    d[sushi[end]] += 1
+    start += 1
+    end += 1
 
 print(result)
